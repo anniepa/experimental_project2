936c070d724ae2e2d1cd16578f3bf8f719c6a253
Robert DeFelice <trombonedrummer@gmail.com>
2018-10-17 14:49:10 -0400

    Added new project for Big game
    Includes kitchen and bus stop scenes w/ triggers for interactable objects

---
+++ BigGame_Project/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour {
+
+    public GameObject textPanel;
+    public GameObject ContinueBtn;
+    public ArrayList text;
+
+	// Use this for initialization
+	void Start () {
+        textPanel.SetActive(false);
+	}
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        textPanel.SetActive(true);
+    }
+
+    
+}
